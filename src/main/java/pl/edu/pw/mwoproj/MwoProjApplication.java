b446a0d7cf611990d6d9ab989961fc7a06bc958b
TPGPL <TPGPL@users.noreply.github.com>
2023-12-01 23:52:39 +0100

    Initial commit

---
+++ src/main/java/pl/edu/pw/mwoproj/MwoProjApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.pw.mwoproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MwoProjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MwoProjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/edu/pw/mwoproj/MwoProjApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.pw.mwoproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MwoProjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
